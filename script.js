855b9bed6c7c07f5f6aa2ed0744cda38d960cbd2
caroltheuri <carolinetheuriwangari2@gmail.com>
2017-05-31 12:26:02 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function carol(den,res){
+  var den=prompt("just work please");
+  var res=den.charAt(0);
+  var res2=den.charAt(den.length-1);
+  var final=res.concat(res2).toUpperCase();
+console.log(res);
+console.log(res2);
+console.log(final);
+
+}
+
+carol();
